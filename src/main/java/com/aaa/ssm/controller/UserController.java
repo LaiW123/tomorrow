0f3aa2775074429202566b1fb01497ff0041e22e
LaiW123 <531460852@qq.com>
2019-03-07 17:15:46 +0800

    我的第一次

---
+++ src/main/java/com/aaa/ssm/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.aaa.ssm.controller;
+
+import com.aaa.ssm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+  @ResponseBody
+    @RequestMapping("/userlist")
+  public Object list(){
+
+       return userService.getUserList();
+
+   }
+}
